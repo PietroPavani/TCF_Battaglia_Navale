d450078e9ec36ca444c463d409c73abec608fbc1
alessandro4colo <164773722+alessandro4colo@users.noreply.github.com>
2024-04-02 15:31:15 +0200

    Add files via upload

---
+++ Player.h
@@ -0,0 +1,41 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <iostream>
+using namespace std;
+#include <iomanip>
+
+#include<math.h>
+#include<cmath>
+
+class Player{
+    
+public:
+
+
+// i primi due valori sono la posizione nella scacchiera il terzo il valore da modificare
+    void setCella(int, int, int );
+
+    void drawScacchiera()const;
+// verifica se le caselle sono libere per ospitare una nave primi due int casella iniziale il terzo 
+// la lunghezza e la stringa india la direzione
+    bool checkCaselle(int,int,int, string)const;
+ 
+private:
+
+    int Scacchiera[10][10]={0};
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
