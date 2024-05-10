a32fd883962c2b95d43a6fbcd3136358adbfed69
PietroPavani <163870071+14235903@users.noreply.github.com>
2024-05-10 11:32:38 +0200

    Update Finale Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 #include <iomanip>
 #include<vector>
+#include<iterator>
 #include<algorithm>
 #include<math.h>
 #include<cmath>
 #include <string>
 #include "Player.h"
+#include "Nave.h"
 
 Player::Player(string nome):Name(nome){};
 
@@ -21,7 +23,19 @@ string Player::traduciCoordinate_in_Lettere(int riga, int colonna) {
     int numero = colonna;
     return string(1, lettera) + to_string(numero); // Componi la stringa con la lettera della riga e il numero della colonna
 }
-
+void Player::traduciLettere_in_Coordinate(int* x,int* y, string coord){
+	char CX = coord[0];
+	char CY = coord[1];
+	*y= (int)CY-48;
+	if((int)CX<75)
+	{
+		*x=((int)CX)-65;
+		}
+	else if ((int)CX>96)
+	{
+		*x=((int)CX)-97;
+	}
+}
 
 
 string Player::getName() const{
@@ -48,20 +62,63 @@ void Player::drawScacchiera()const{
 	}
 
 }
-// per adesso controllo solo nella direzione destra
+
 bool Player::checkCaselle(int x, int y, int size, string direction)const{
 	bool result=true;
-	if(x+size>=10){
-		result=false;
-	}
-	for(int i=0;i<size;i++){
-		if(Scacchiera[x+i][y]!=0){
+	//qui potremmo fare controllo nel caso si siano date coordinate sbagliate ecc con un return e una stampa
+	if(direction=="Est"||direction=="est"){
+		if(x+size>=10){
 			result=false;
+			}
+	else{
+		for(int i=0;i<size;i++){
+			if(Scacchiera[x+i][y]!=0){
+				result=false;
+				}	
+			}
 		}
 	}
+	if(direction=="Ovest"||direction=="ovest"){
+		if(x-size<0){
+			result=false;
+			}
+	else{
+		for(int i=0;i<size;i++){
+			if(Scacchiera[x-i][y]!=0){
+				result=false;
+				}	
+			}
+		}
+	}	
+	if(direction=="Nord"||direction=="nord"){
+		if(y+size>=10){
+			result=false;
+			}
+	else{
+		for(int i=0;i<size;i++){
+			if(Scacchiera[x][y+i]!=0){
+				result=false;
+				}	
+			}
+		}
+	}
+	if(direction=="Sud"||direction=="sud"){
+		if(x-size<0){
+			result=false;
+			}
+	else{
+		for(int i=0;i<size;i++){
+			if(Scacchiera[x][y-i]!=0){
+				result=false;
+				}	
+			}
+		}
+	}	
 
+	
 	return result;
 }
+
 
 void Player::shooting(Player &p){
 	
@@ -72,8 +129,9 @@ void Player::shooting(Player &p){
 	string coord;
 	// mettere il codice per nome giocatore
 	bool test=false;
-	do{cout << "\n Inserire coordinate che vuoi colpire nel formato tipo A1\n";
+	do{cout << "\n Casella: inserire coordinate nel formato tipo A1\n";
 	cin >> coord;
+	this->traduciLettere_in_Coordinate(&x,&y,coord);
 	// eventualmente aggiungere check se il formato della stringa è corretto
 	if(p.Scacchiera[x][y] == 2 || p.Scacchiera[x][y] == 3){
 		cout << "\n La Casella è già stata colpita \n";
@@ -89,18 +147,106 @@ void Player::shooting(Player &p){
 	if(p.Scacchiera[x][y] == 1){
 		p.setCella(x,y, value_colpito);
 		// Volendo aggiungere coordinata colpita con eventuale traduzione in lettere
-		cout << coord <<": Colpito!\n" << endl; // ho aggiunto il cout di coord, non so se funzioni
- 	}
+		cout << "\nColpito!\n" << endl;
+	}
 	if(p.Scacchiera[x][y] == 0){
 		p.setCella(x,y, value_mancato);
-		cout << coord << "\nMancato!\n" << endl; // come sopra
+		cout << "\nMancato!\n" << endl;
 	}
 }
 
-void Player::addNave(Nave* ship){
-	Flotta.push_back(ship);
+
+void Player::createFleet(){
+	int x;
+	int y;
+	string coord;
+	string dir;
+	bool test;
+	for(int l = 1; l<5;l++)
+	{
+		do
+		{
+			cout<<"inserire le coordinate per la Lancia numero "<<l<< " di dimensione 1";
+			cin>>coord;
+			this->traduciLettere_in_Coordinate(&x,&y,coord);
+			cout<<"inserire la direzione";
+			cin>>dir;
+			test=this->checkCaselle(x,y,1,dir);	
+		}
+		while(!test);
+		Flotta.push_back(new Lancia(&Scacchiera[x][y],dir));
+	}
+	for(int t = 1; t<4;t++)
+	{
+		do
+		{
+			cout<<"inserire le coordinate per la Torpediniera numero "<<t<< " di dimensione 2";
+			cin>>coord;
+			this->traduciLettere_in_Coordinate(&x,&y,coord);
+			cout<<"inserire la direzione";
+			cin>>dir;
+			test=this->checkCaselle(x,y,2,dir);	
+		}
+		while(!test);
+		Flotta.push_back(new Torpediniera(&Scacchiera[x][y],dir));
+	}
+	for(int s = 1; s<3;s++)
+	{
+		do
+		{
+			cout<<"inserire le coordinate per la Lancia numero "<<s<< " di dimensione 3";
+			cin>>coord;
+			this->traduciLettere_in_Coordinate(&x,&y,coord);
+			cout<<"inserire la direzione";
+			cin>>dir;
+			test=this->checkCaselle(x,y,3,dir);	
+		}
+		while(!test);
+		Flotta.push_back(new Sottomarino(&Scacchiera[x][y],dir));
+	}
+	for(int c = 1; c<2;c++)
+	{
+		do
+		{
+			cout<<"inserire le coordinate per la Corazzata numero "<<c<< " di dimensione 4";
+			cin>>coord;
+			this->traduciLettere_in_Coordinate(&x,&y,coord);
+			cout<<"inserire la direzione";
+			cin>>dir;
+			test=this->checkCaselle(x,y,4,dir);	
+		}
+		while(!test);
+		Flotta.push_back(new Corazzata(&Scacchiera[x][y],dir));
+	}
+
+
+
+
+
+	//Flotta.push_back(new Torpediniera(&Scacchiera[5][5],"ovest"));
 }
 
-void Player::posizionamento(){
-	// manca implementazione
+void Player::updateFleet(){
+	vector<Nave*>::iterator iter;
+
+	for(iter=Flotta.begin(); iter!=Flotta.end(); iter++) {
+		 (*(*iter)).calcoloDanni();
+ }
+}
+
+bool Player::checkDefeat(){
+vector<Nave*>::iterator iter;
+	int x = Flotta.size();
+	for(iter=Flotta.begin(); iter!=Flotta.end(); iter++) {
+
+		if( (*(*iter)).getAffondato()){
+				x--;
+		}
+	}
+if(x==0){
+	return true;
+}
+else{
+	return false;
+}
 }
