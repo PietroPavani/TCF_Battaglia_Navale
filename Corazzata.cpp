01c8275d51dd269d37d09d2f2ade4fc8fa3f9970
PietroPavani <163870071+14235903@users.noreply.github.com>
2024-05-10 11:47:25 +0200

    Update Corazzata.cpp

--- Corazzata.cpp
+++ Corazzata.cpp
@@ -6,31 +6,8 @@
 #include<math.h>
 #include<cmath>
 #include <vector>
+#include "Nave.h"
 using std::string;
 using namespace std;
 
-Corazzata::Corazzata(int &P, string D, int L, const string& naveType):Direzione(D),Size(4),type("Corazzata"),Affondato(false),StartPosition(&P)
-{}
-void Corazzata::calcoloDanni(){
-    int x=Size;
-    for(int i=0;i<Size;i++){
-        if(*Position[i]==2){
-            x--;
-        }
-    }
-    if(x==0){
-        Affondato = true;
-    }
-}
-
-bool Corazzata::getAffondato()const{
-    return Affondato;
-}
-int Corazzata::getSize() const {
-    return Size;
-}
-char Corazzata::getSimbolo() const {
-    return 'C';
-}
-
-
+Corazzata::Corazzata(int *P, string D) : Nave(P, D, 4, 'C') {}
