e6895231b34b140b3a2ef158bbe2c677331f6303
alessandro4colo <164773722+alessandro4colo@users.noreply.github.com>
2024-05-10 11:43:43 +0200

    Update vtt Lancia.cpp

--- Lancia.cpp
+++ Lancia.cpp
@@ -9,26 +9,4 @@
 using std::string;
 using namespace std;
 
-Lancia::Lancia(int &P, string D, int L, const string& naveType):Direzione(D),Size(1),type("Lancia"),Affondato(false),StartPosition(&P)
-{}
-void Lancia::calcoloDanni(){
-    int x=Size;
-    for(int i=0;i<Size;i++){
-        if(*Position[i]==2){
-            x--;
-        }
-    }
-    if(x==0){
-        Affondato = true;
-    }
-}
-
-bool Lancia::getAffondato()const{
-    return Affondato;
-}
-int Lancia::getSize() const {
-    return Size;
-}
-char Lancia::getSimbolo() const {
-    return 'L';
-}
+Lancia::Lancia(int *P, string D) : Nave(P, D, 1, 'L') {}
