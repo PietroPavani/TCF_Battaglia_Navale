e30aba81f742812dbf7d14b805a9bb5b5127f33a
alessandro4colo <ale.4colo@gmail.com>
2024-05-17 22:18:46 +0200

    shooting computer iniziato

--- Computer.cpp
+++ Computer.cpp
@@ -21,23 +21,54 @@ Computer::Computer(string nome):Name(nome),Status(0){};
 
 
 void Computer::shooting(Player &p){
-switch(Status){
     //colpisce a random (da finire)
-    case 0:
-    srand(time(NULL));
-    int x = rand()%10;
-    int y = rand()%10;
-    break;
+if(Status==0){
+srand(time(NULL));
+int x;
+int y;
+const int value_colpito = 2; 
+const int value_mancato = 3;  
+
+bool test=false;
+	do{
+    x = rand()%10;
+    y = rand()%10;
+	if(p.getCella(x,y) == 2 || p.getCella(x,y) == 3){
+		test=true;
+	}
+	if(p.getCella(x,y) == 0 ||p.getCella(x,y)== 1){
+		test=false;
+	}
+	}	
+	while(test);
+
+	if(p.getCella(x,y) == 1){
+		p.setCella(x,y, value_colpito);
+		// Volendo aggiungere coordinata colpita con eventuale traduzione in lettere
+		cout << "\nColpito!\n" << endl;
+        XLastShot=x;
+        YLastShot=y;
+        Status=1;
+	}
+	if(p.getCella(x,y) == 0){
+		p.setCella(x,y, value_mancato);
+		cout << "\nMancato!\n" << endl;
+	}
+
+}
+
     //spara vicino in direzione casuale
-    case 1:
+if(Status==1){
 
-    break;
+    
+}
     //continua a sparare nella stessa direzione
-    case 2:
+if(Status==2){
 
-    break;
+    
+}
     
 
 }
-}
+
 
